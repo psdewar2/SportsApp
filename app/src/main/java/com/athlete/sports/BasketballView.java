dce31d3ab2bcd466dc0b7b16ef1509311f8fba8b
psdewar2 <psdewar2@live.com>
2015-01-07 03:43:47 -0500

    added color to sports buttons and an intent to map view screen

---
+++ app/src/main/java/com/athlete/sports/BasketballView.java
@@ -0,0 +1,14 @@
+package com.athlete.sports;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by PSD on 1/7/15.
+ */
+public class BasketballView extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basketball);
+    }
+}
--- app/src/main/java/com/athlete/sports/MainActivity.java
+++ app/src/main/java/com/athlete/sports/MainActivity.java
@@ -1,9 +1,12 @@
 package com.athlete.sports;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.parse.Parse;
 import com.parse.ParseException;
@@ -14,10 +17,10 @@ import com.parse.ui.ParseLoginBuilder;
 
 public class MainActivity extends ActionBarActivity {
 
+    private Button bball, ftball, sccr, mLogOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         Parse.initialize(this, getString(R.string.parse_app_id), getString(R.string.parse_client_key));
 
         //test user
@@ -39,5 +42,15 @@ public class MainActivity extends ActionBarActivity {
         ParseLoginBuilder loginBuilder = new ParseLoginBuilder(MainActivity.this);
         startActivityForResult(loginBuilder.build(), 0);
 
+        setContentView(R.layout.activity_main);
+        bball = (Button) findViewById(R.id.bball);
+        bball.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, BasketballView.class);
+                startActivity(i);
+            }
+        });
     }
 }
