885c7920f5d9ce83356374cd12e9c9b6cffc6eec
psdewar2 <psdewar2@live.com>
2015-01-07 02:35:47 -0500

    Added login and sign up functionality

--- app/src/main/java/com/athlete/sports/MainActivity.java
+++ app/src/main/java/com/athlete/sports/MainActivity.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+import com.parse.ui.ParseLoginBuilder;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,28 +18,26 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        Parse.initialize(this, getString(R.string.parse_app_id), getString(R.string.parse_client_key));
 
+        //test user
+        ParseUser user = new ParseUser();
+        user.setUsername("psd");
+        user.setPassword("1234567");
+        user.setEmail("email@example.com");
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    // Hooray! Let them use the app now.
+                } else {
+                    // Sign up didn't succeed. Look at the ParseException
+                    // to figure out what went wrong
+                }
+            }
+        });
+        //initializes login interface
+        ParseLoginBuilder loginBuilder = new ParseLoginBuilder(MainActivity.this);
+        startActivityForResult(loginBuilder.build(), 0);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
